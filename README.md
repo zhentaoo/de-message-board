694275a42bf88bcc9a3d9170211849df42edde6d
zhentaoo <zhentaoo666@gmail.com>
2024-12-03 22:53:40 +0800

    done: board message

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 - forge build
 - forge script script/Deploy.s.sol --rpc-url https://eth-sepolia.g.alchemy.com/v2/5I4kYX9e4XywtB2PcBf2EGrrSftoVN0G --private-key e39435ae7939c5a8f1990a6db2cd3cd985c858f084a52b6553b2205df5a63fd4 --broadcast
 
-- 这个合约已经发布成功，地址
-    - 0x9bb3c5e443787e25d0b3ad77042ca7d3422c1682e1083963f602b9aa4a4c595b
-    - Contract Address: 0xE9A70C9e3502a98a47d37Bb8E90751AB12ece8e7
+- 这个合约已经发布成功，地址 Contract Address: 0xE9A70C9e3502a98a47d37Bb8E90751AB12ece8e7
+
+- 查看合约地址： https://sepolia.etherscan.io/address/0xE9A70C9e3502a98a47d37Bb8E90751AB12ece8e7#code
+
+- 查看通过合约留言的地址：https://sepolia.etherscan.io/address/0x1787cb48812b36Dc200e32c189e0cF472d2b0f5F
+
+ 
--- fe/src/service/contract.js
+++ fe/src/service/contract.js
@@ -2,11 +2,18 @@ import { ethers } from "ethers";
 import MessageBoardABI from "../abi/MessageBoard.json";
 
 const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
-const provider = new ethers.providers.JsonRpcProvider(process.env.REACT_APP_ALCHEMY_URL);
-const contract = new ethers.Contract(contractAddress, MessageBoardABI, provider);
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.REACT_APP_ALCHEMY_URL
+);
+const contract = new ethers.Contract(
+  contractAddress,
+  MessageBoardABI,
+  provider
+);
 
 export const getMessages = async () => {
   const messages = await contract.getMessages();
+  debugger
   return messages.map((msg) => ({
     sender: msg.sender,
     content: msg.content,
@@ -15,8 +22,15 @@ export const getMessages = async () => {
 };
 
 export const postMessage = async (content) => {
+  debugger
   const privateKey = process.env.REACT_APP_PRIVATE_KEY; // 为测试私钥
+  console.log("privateKey:", privateKey);
+  console.log('Environment Variables:', process.env);
+
+
   const wallet = new ethers.Wallet(privateKey, provider);
+  console.log("wallet:", wallet);
+
   const contractWithSigner = contract.connect(wallet);
 
   const tx = await contractWithSigner.postMessage(content);
